b144772c30a8450f0838948317a34fd4c83340e9
JT Skidmore <johntoddskidmore@gmail.com>
2020-05-02 00:29:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
   while count < src.count do
     
    src[count].sort 
-   lowest_nums << src[count].first 
+   lowest_nums << src[count].last 
     
    count += 1
     
